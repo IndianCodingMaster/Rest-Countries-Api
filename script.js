dfe891b5e40652694885ebb67d80db2ed92a457d
Satya Prakash <satyaprakash91180@gmail.com>
2024-03-11 23:16:59 +0530

    adding more features

--- script.js
+++ script.js
@@ -1,37 +1,53 @@
 const cardContainer = document.querySelector(".card-container");
+const searchInput = document.querySelector("#input");
+const filterByRegion = document.querySelector(".filter-by-region");
+const themeChanger = document.querySelector(".theme-changer");
 
-// const cardHTML = `
-//     <img src="https://flagcdn.com/in.svg" alt="flag" width="200" />
-//     <div class="country-details">
-//       <h3>Germany</h3>
-//       <p><b>Population : </b><span>8155555</span></p>
-//       <p><b>Region : </b><span>Europe</span></p>
-//       <p><b>Capital : </b><span>Berlin</span></p>
-//     </div>
-//   `;
-// cardContainer.innerHTML = cardHTML;
-
-// console.log(cardContainer);
+let allCountriesData;
 
 fetch("https://restcountries.com/v3.1/all")
   .then((res) => res.json())
   .then((data) => {
-    data.forEach((country) => {
-      const countryCard = document.createElement("a");
-      countryCard.href = `/country.html?name=${country.name.common}`;
-      countryCard.classList.add("card");
-      countryCard.innerHTML = `
-        <img src=${country.flags.svg} alt="flag" />
-        <div class="country-details">
-          <h3>${country.name.common}</h3>
-          <p><b>Population : </b><span>${country.population.toLocaleString(
-            "en-IN"
-          )}</span></p>
-          <p><b>Region : </b><span>${country.region}</span></p>
-          <p><b>Capital : </b><span>${country.capital}</span></p>
-        </div>
-       `;
-      cardContainer.append(countryCard);
-      // console.log(countryCard);
-    });
+    renderCountries(data);
+    allCountriesData = data;
   });
+
+filterByRegion.addEventListener("change", (e) => {
+  fetch(`https://restcountries.com/v3.1/region/${filterByRegion.value}`)
+    .then((res) => res.json())
+    .then((data) => {
+      renderCountries(data);
+    });
+});
+
+function renderCountries(data) {
+  cardContainer.innerHTML = "";
+  data.forEach((country) => {
+    const countryCard = document.createElement("a");
+    countryCard.href = `/country.html?name=${country.name.common}`;
+    countryCard.classList.add("card");
+    countryCard.innerHTML = `
+      <img src=${country.flags.svg} alt="flag" />
+      <div class="country-details">
+        <h3>${country.name.common}</h3>
+        <p><b>Population : </b><span>${country.population.toLocaleString(
+          "en-IN"
+        )}</span></p>
+        <p><b>Region : </b><span>${country.region}</span></p>
+        <p><b>Capital : </b><span>${country.capital}</span></p>
+      </div>
+     `;
+    cardContainer.append(countryCard);
+  });
+}
+
+searchInput.addEventListener("input", (e) => {
+  const filteredCountries = allCountriesData.filter((country) =>
+    country.name.common.toLowerCase().includes(e.target.value.toLowerCase())
+  );
+  renderCountries(filteredCountries);
+});
+
+themeChanger.addEventListener("click", () => {
+  document.body.classList.toggle("dark");
+});
